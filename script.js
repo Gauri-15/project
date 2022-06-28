ecb0eeab6db3bdd093567db63d159f35ef7dc422
ApurvaP-10 <106000778+ApurvaP-10@users.noreply.github.com>
2022-06-28 03:41:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementsById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');   
+    }
+    )
+}
